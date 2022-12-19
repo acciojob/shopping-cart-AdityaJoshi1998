6aa3c1569cdc13bad3c2c63d8d47de56bb9d7343
AdityaJoshi1998 <adijoshi1098@gmail.com>
2022-12-19 13:13:58 +0530

    completed code

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
 //your code here
-let item = document.getElementsByTagName("input")[0];
-let price = document.getElementsByTagName("input")[1];
-let button = document.getElementsByTagName("button")[0];
-let container = document.getElementsByClassName("container")[0];
-let ss = document.getElementsByTagName('h3')[0];
-let str = "";
-let arr = [];
-function addRow() {
-   
-  if (item.value == "" || price.value == "") {
-     ss.innerText = "All fields required";
-  }
-  else if(item.validity.typeMismatch || price.validity.typeMismatch){
-     ss.innerText = "Please insert valid input";
-  }
-
-else {
-    str = str + "+" + price.value;
-    ss.innerText = "";
-    let table = document.getElementById("table");
-   let row = table.insertRow(-1); 
-    let c1 = row.insertCell(0);
-    let c2 = row.insertCell(1);
-   c1.innerText = item.value;
-    c2.innerText = price.value;
-    item.value = "";
-    price.value = "";
-    ss.innerText = "Total -:    " + eval(str);
-  }
+let grandtotal = 0;
+const addData = () => {
+  let name = document.getElementById('itemname').value;
+  let quantity = document.getElementById('itemquantity').value;
+  let price = document.getElementById('itemprice').value;
+  let table = document.getElementById('table');
+  let newRow = table.insertRow(table.rows.length - 1);
+  let namecell = newRow.insertCell(0);
+  let quantitycell = newRow.insertCell(1);
+  let pricecell = newRow.insertCell(2);
+  let totalprice = newRow.insertCell(3);
+  namecell.classList.add("item");
+  totalprice.classList.add("price");
+  namecell.innerHTML = name;
+  quantitycell.innerHTML = quantity;
+  pricecell.innerHTML = price;
+  totalprice.innerHTML = quantity * price;
+  grandtotal += quantity * price;
+  document.getElementById('total').innerHTML = "Total Ammount " + grandtotal + " RS";
+  document.getElementById('input').reset();
 }
-console.log(arr);
-button.addEventListener("click", addRow);
+
+module.exports = addData;
