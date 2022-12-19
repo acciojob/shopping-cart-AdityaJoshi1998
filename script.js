a29d9e40eb8761f4b3b04ad326c42b1a785d2abb
AdityaJoshi1998 <adijoshi1098@gmail.com>
2022-12-19 13:02:05 +0530

    completed code

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+let item = document.getElementsByTagName("input")[0];
+let price = document.getElementsByTagName("input")[1];
+let button = document.getElementsByTagName("button")[0];
+let container = document.getElementsByClassName("container")[0];
+let ss = document.getElementsByTagName('h3')[0];
+let str = "";
+let arr = [];
+function addRow() {
+   
+  if (item.value == "" || price.value == "") {
+     ss.innerText = "All fields required";
+  }
+  else if(item.validity.typeMismatch || price.validity.typeMismatch){
+     ss.innerText = "Please insert valid input";
+  }
+
+else {
+    str = str + "+" + price.value;
+    ss.innerText = "";
+    let table = document.getElementById("table");
+   let row = table.insertRow(-1); 
+    let c1 = row.insertCell(0);
+    let c2 = row.insertCell(1);
+   c1.innerText = item.value;
+    c2.innerText = price.value;
+    item.value = "";
+    price.value = "";
+    ss.innerText = "Total -:    " + eval(str);
+  }
+}
+console.log(arr);
+button.addEventListener("click", addRow);
